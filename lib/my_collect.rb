3c3c3bad3d38188d1f502330f5c01509fea7d0f4
Jack Robbins <jackrobbins1@gmail.com>
2019-03-21 05:16:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(arr)
   n = 0
-  new_arrayz = []
+  new_array = []
   while n < arr.length
-    yield arr[n]
+    new_array << yield arr[n]
     n += 1
   end
-  new_arrayz
+  new_array
 end
